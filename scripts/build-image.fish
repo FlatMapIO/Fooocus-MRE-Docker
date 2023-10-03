ab0d7fc699fcd6faa65863246ac0ec42d0c45034
huodon <huodon@gmail.com>
2023-10-03 12:39:22 +0800

    WIP

--- scripts/build-image.fish
+++ scripts/build-image.fish
@@ -1,3 +1,17 @@
 #!/usr/bin/env fish
 
-docker compose build {comfy-ui, fooocus-mre}
+if test (count $argv) -ne 1
+    echo "Usage: build-image.fish <ComfyUI|Fooocus-MRE>"
+    exit 1
+end
+
+set app (string lower $argv[1])
+
+switch $app
+    case comfyui
+        docker compose build comfy-ui
+    case fooocus-mre
+        docker compose build fooocus-mre
+    case '*'
+        docker compose build {comfy-ui, fooocus-mre}
+end
--- scripts/prepare.fish
+++ scripts/prepare.fish
@@ -1,8 +1,11 @@
 #!/usr/bin/env fish
-function clone-up
+
+set ROOT_DIR (pwd)
+
+function clone_up
 
     if test (count $argv) -ne 2
-        echo "Usage: clone-up  <repository_url> <directory>"
+        echo "Usage: clone_up  <repository_url> <directory>"
         exit 1
     end
 
@@ -22,19 +25,24 @@ function clone-up
 end
 
 
-function install-fooocus-mre_comfyui
 
-    set COMFY_COMMIT_HASH 2381d36e6db8e8150e42ff2ede628db5b00ae26f
 
-    if test -d ./mount/repositories/ComfyUI-from-StabilityAI-Official
-        echo '✅ ./mount/repositories/ComfyUI-from-StabilityAI-Official exists'
-        return
+function fooocus_mre_postinstall
+    if not test -d ./mount/repositories/ComfyUI-from-StabilityAI-Official
+        git clone --depth 1 https://github.com/comfyanonymous/ComfyUI ./mount/repositories/ComfyUI-from-StabilityAI-Official
     end
 
-    clone-up ./mount/repositories/ComfyUI-from-StabilityAI-Official https://github.com/comfyanonymous/ComfyUI
     cd ./mount/repositories/ComfyUI-from-StabilityAI-Official
+    git fetch
+
+    set COMFY_COMMIT_HASH \
+        (curl -sSL https://raw.githubusercontent.com/MoonRide303/Fooocus-MRE/moonride-main/launch.py \
+        |  rg  -o '\'COMFY_COMMIT_HASH\',\s"([0-9a-f]{40})"\)' -r '$1')
+
+    echo "COMFY_COMMIT_HASH: $COMFY_COMMIT_HASH"
+
     git checkout $COMFY_COMMIT_HASH
-    prevd
+    cd $ROOT_DIR
 end
 
 function fooocus_sync_prompt_expansion
@@ -44,39 +52,30 @@ function fooocus_sync_prompt_expansion
         exit 1
     end
 
-    rsync --ignore-existing -av \
+    rsync -av \
         Fooocus-MRE/models/prompt_expansion/fooocus_expansion/ \
-        mount/models/prompt_expansion/fooocus_expansion/
-    echo "✅ mount/models/prompt_expansion/fooocus_expansion/ synced"
+        mount/storage/models/prompt_expansion/fooocus_expansion/
+    echo "✅ models/prompt_expansion/fooocus_expansion synced"
 end
 
-function install-fooocus-mre
-    clone-up https://github.com/MoonRide303/Fooocus-MRE.git Fooocus-MRE
-    install-fooocus-mre_comfyui
+function install_fooocus_mre
+    clone_up https://github.com/MoonRide303/Fooocus-MRE.git Fooocus-MRE
+    fooocus_mre_postinstall
     fooocus_sync_prompt_expansion
 end
 
 
-
-function install-comfy-ui
-    clone-up https://github.com/comfyanonymous/ComfyUI.git ComfyUI
-
+function install_comfy_ui
+    clone_up https://github.com/comfyanonymous/ComfyUI.git ComfyUI
+    clone_up https://github.com/ltdrdata/ComfyUI-Manager.git ./mount/custom_nodes/ComfyUI-Manager
     # Custom nodes
-    clone-up https://github.com/ltdrdata/ComfyUI-Manager.git            ./mount/custom_nodes/ComfyUI-Manager
-    clone-up https://github.com/ltdrdata/ComfyUI-Impact-Pack.git        ./mount/custom_nodes/ComfyUI-Impact-Pack
-    clone-up https://github.com/ltdrdata/ComfyUI-Inspire-Pack.git       ./mount/custom_nodes/ComfyUI-Inspire-Pack
-    clone-up https://github.com/Fannovel16/comfyui_controlnet_aux.git   ./mount/custom_nodes/Comfyui-Controlnet-Auxs
-    # clone-up https://github.com/BlenderNeko/ComfyUI_TiledKSampler.git ./mount/custom_nodes/ComfyUI_TiledKSample
-    clone-up https://github.com/WASasquatch/FreeU_Advanced.git          ./mount/custom_nodes/FreeU_Advanced
+    clone_up https://github.com/ltdrdata/ComfyUI-Impact-Pack.git ./mount/custom_nodes/ComfyUI-Impact-Pack
+    clone_up https://github.com/ltdrdata/ComfyUI-Inspire-Pack.git ./mount/custom_nodes/ComfyUI-Inspire-Pack
+    clone_up https://github.com/Fannovel16/comfyui_controlnet_aux.git ./mount/custom_nodes/Comfyui-Controlnet-Auxs
 
-    # clone-up https://github.com/laksjdjf/IPAdapter-ComfyUI.git  ./mount/custom_nodes/IPAdapter-ComfyUI
-    clone-up https://github.com/cubiq/ComfyUI_IPAdapter_plus.git        ./mount/custom_nodes/ComfyUI_IPAdapter_plus
-    clone-up https://github.com/WASasquatch/was-node-suite-comfyui.git  ./mount/custom_nodes/was-node-suite-comfyui
-    clone-up https://github.com/SeargeDP/SeargeSDXL.git                 ./mount/custom_nodes/SeargeSDXL
-    # clone-up https://github.com/BlenderNeko/ComfyUI_Cutoff ./mount/custom_nodes/ComfyUI_Cutoff
-    # clone-up https://github.com/Fannovel16/ComfyUI-Frame-Interpolation.git ./mount/custom_nodes/ComfyUI-Frame-Interpolation
-
+    clone_up https://github.com/cubiq/ComfyUI_IPAdapter_plus.git ./mount/custom_nodes/ComfyUI_IPAdapter_plus
+    clone_up https://github.com/SeargeDP/SeargeSDXL.git ./mount/custom_nodes/SeargeSDXL
 end
 
-install-fooocus-mre
-install-comfy-ui
+install_fooocus_mre
+install_comfy_ui
