a8465efc9f041e3ff2c70a453e05d5cf1b61d0f8
huodon <huodon@gmail.com>
2023-10-16 10:19:39 +0000

    WIP

--- scripts/init-env.sh
+++ scripts/init-env.sh
@@ -4,3 +4,12 @@ echo '>>> config git'
 
 git config --global user.name huodon
 git config --global user.email huodon@gmail.com
+
+
+fish_exec=$(which fish)
+
+if [[ "$fish_exec" = "" ]]; then
+  echo ">>> install fish"
+  apt update
+  apt install fish
+fi
